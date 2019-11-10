132605ad9cb8de67ac7049f8552035153cf50933
SuperMayy <mayowashowumi@outlook.com>
2019-11-10 22:51:27 +0000

    Updates

---
+++ precache-manifest.1fd3ba611fcacdb57f3762b7ae9ca27b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44d9f0768de45a3d1038edd62ce285a8",
+    "url": "/youtube-clone-deployed/index.html"
+  },
+  {
+    "revision": "e7579cac4acf5881f239",
+    "url": "/youtube-clone-deployed/static/js/2.17ca2a73.chunk.js"
+  },
+  {
+    "revision": "69fe60b78a8789f70efb",
+    "url": "/youtube-clone-deployed/static/js/main.8ae19a7a.chunk.js"
+  },
+  {
+    "revision": "0c18ed990f629be4d0fc",
+    "url": "/youtube-clone-deployed/static/js/runtime-main.fca62202.js"
+  }
+]);
